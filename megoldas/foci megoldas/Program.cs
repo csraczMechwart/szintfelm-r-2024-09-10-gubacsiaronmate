6d0e26041fb0b71e103da9181f2309e67c989327
aronmategubacsi <21.gubacsi.aron.mate@mechwart.com>
2024-09-10 12:42:56 +0200

    initial commit

---
+++ megoldas/foci megoldas/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+// ReSharper disable InconsistentNaming
+
+namespace foci_megoldas
+{
+    internal static class Program
+    {
+        private static void Main()
+        {
+            StreamReader inputFile = new("meccs.txt");
+            
+        }
+    }
+}
